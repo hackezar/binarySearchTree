95a288966f3d68c09c949d7da4c64675e792873d
hackezar <107069171+hackezar@users.noreply.github.com>
2024-03-29 14:16:37 -0400

    Progress

--- src/bst.js
+++ src/bst.js
@@ -14,23 +14,32 @@ export class Tree {
   }
 }
 
+export function getNext(arr) {
+  console.log(arr);
+  if (arr.length < 2) 
+    return arr[0];
+  let half = Math.ceil(arr.length/2);
+  let left = arr.splice(0, half);
+  let right = arr;
+  if (left.length > right.length)
+    Tree.root.left = new Node(left[left.length - 1]);
+  else if (left.length <= right.length)
+    return new Node(right[0]);
+  
+}
+
 export function buildTree(array) {  
   let half = Math.ceil(array.length/2);
   let left = array.splice(0, half);
   let right = array;
  if (left.length > right.length) {
-    let tree = new Tree(new Node(left[left.length - 1]));
-    left.splice(half, 1);
-    tree.root.left = getNext(left);
-    return tree;
+    new Tree(new Node(left[left.length - 1]));
+    left.splice(half - 1, 1);
+    Tree.root.left = getNext(left);
+    Tree.root.right = getNext(right);
+    return Tree;
  } else if (left.length < right.length || left.length == right.length) {
   return new Tree(new Node(right[0]));
  }
 }
 
-export function getNext(arr) {
-  if (arr.length < 2) 
-    return arr[0];
-  
-  
-}
--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import "./style.css";
 import { mergeSort, removeDuplicates } from "./mergeSort";
 import { Node, Tree, buildTree } from "./bst";
 import { list } from "./data";
+import { prettyPrint } from "./prettyPrint";
+
 
 // Merge the array into numerical order
 let array = mergeSort(list);
 
 // Remove duplicates
 removeDuplicates(array);
-
+console.log(array);
 // Build the tree with the list
-let tree = (buildTree(array));
-console.log(tree);
+buildTree(array);
+console.log(Tree);
+//prettyPrint(tree.root);
 
